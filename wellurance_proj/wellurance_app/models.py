8b0e8e670365dd93f2dfa31833661d197c09fd42
swawiye <sovereign.wawiye@gmail.com>
2025-07-21 11:46:02 +0300

    notification model

--- wellurance_proj/wellurance_app/models.py
+++ wellurance_proj/wellurance_app/models.py
@@ -145,3 +145,24 @@ class LocationUpdate(models.Models):
             models.Index(fields=['responder']),
             models.Index(fields=['time_stamp']),
         ]
+
+class Notification(models.Model):
+    NOTIFICATION_TYPES = (
+        'EMERGENCY_ASSIGNED', 'Emergency Assigned',
+        'STATUS_UPDATE', 'Status Update',
+        'SYSTEM_ALERT', 'System Alert',
+    )
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='notifications')
+    notification_type = models.CharField(max_length=20, choices=NOTIFICATION_TYPES)
+    title = models.CharField(max_length=100)
+    message = models.TextField()
+    related_incident = models.ForeignKey(EmergencyReport, on_delete=models.SET_NULL, null=True, blank=True)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['user', 'is_read']),
+        ]
